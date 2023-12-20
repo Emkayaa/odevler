531c693f4e929feea56dc34f5a69412e4ac41b41
Emkayaa <147692298+Emkayaa@users.noreply.github.com>
2023-12-20 20:53:42 +0300

    Delete python directory

--- python/01ödev/python/Fonksiyon ucgen.py
+++
@@ -1,9 +0,0 @@
-def ucgen(a):
-    for i in range(1, a + 1):
-        print('*' * i)
-
-try:
-    a = int(input("Değer girin: "))
-    ucgen(a)
-except ValueError:
-    print("sayı yada rakam girin")
